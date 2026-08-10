ed34872cc03400b951fedd3cc8335f443904d789
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2026-08-10 11:12:07 +0700

    update

--- modules.blade.php
+++ modules.blade.php
@@ -19,3 +19,36 @@ use_module([
   'layanan' => true,
   'faq'=>true
 ]);
+
+add_default_polling_topic([
+    'penilaian_public' => [
+        'title' => 'Bagaimana Kualitas Website Kami saat ini ?',
+        'description' => 'Untuk jajak pendapat tentang kualitas website',
+        'status' => 'publish',
+        'duration' => 60,
+        'option' => ['Buruk', 'Cukup Baik', 'Sangat Baik']
+    ]
+]);
+add_default_category(['menu'=>['Header','Footer'],'banner'=>['Home','Sidebar','Header','Banner Berita','Popup']]);
+use_module([
+    'sambutan'=>true,
+    'download'=>['active'=>true,'web'=>['auto_query'=>true,'post_perpage'=>20],'datatable'=>['custom_column'=>'File']] ,
+    'pengumuman'=>true,
+    'kepegawaian'=>['form'=>['post_parent'=>false]],
+    'layanan'=>true,
+	'unit-kerja'=>true,
+	'agenda'=>true,
+    'galeri'=>true,
+	'faq'=>['web'=>['detail'=>false],'datatable'=>['custom_column'=>['Jawaban']],'form'=>['editor'=>false,'custom_field'=>[['Jawaban',['type'=>'rich-text']]]]]
+]);
+
+add_option('template',[
+['Tahun Pembuatan','text']
+]);
+
+use_module([
+    'page'=>['form'=>['custom_field'=>[
+        ['Lampiran',['type'=>'file','mime_type'=>['image/webp','application/pdf']]]
+    ],
+    'looping_name'=>'Info Halaman','looping_data'=>[['Label','text'],['Keterangan','textarea']]]]
+]);
